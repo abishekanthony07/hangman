e915b78cd00ce56f0590f6ff67ff63396268e4a2
Tim Schuetzle <Tim.Schuetzle@n-komm.de>
2021-05-29 10:18:06 +0200

    BLI_2 - Erstinitialisierung Spring Boot Projekt

---
+++ src/test/java/com/GenarroSEProjekt/DG/AA/TS/AG/hangman/HangmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GenarroSEProjekt.DG.AA.TS.AG.hangman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
